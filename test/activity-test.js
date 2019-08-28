ee6a456fec6b4619128109d9686fdd981ea62bef
Allison Wagner <allisonjw01@gmail.com>
2019-08-28 11:46:16 -0600

    Fixes spelling errors in it block

--- test/activity-test.js
+++ test/activity-test.js
@@ -22,25 +22,25 @@ describe('Activity', () => {
     expect(activity).to.be.an.instanceof(Activity);
   });
 
-  it('shoud be able to tell how many steps taken on a specific day', () =>{
+  it('shoud be able to tell how many steps were taken on a specific day', () =>{
     expect(activity.userStepsPerDay(1, '2019/06/15')).to.equal(3577);
   });
 
-  it('shoud be able to tell how many miles user walked based on steps taken on a specific day', () =>{
+  it('shoud be able to return the miles for the user based on steps for a specific day', () =>{
     expect(activity.milesUserWalked(1, '2019/06/15', userData)).to.equal(2.9);
   });
 
-  it('shoud be know how minutes a user was active, based on their id, on a specified', () =>{
+  it('shoud be know how minutes a user was active for a specific day', () =>{
     expect(activity.minsUserActive(1, '2019/06/15')).to.equal(140);
 
   });
 
-  it('shoud now how many minutes active was their average for a given week', () =>{
+  it('shoud now how many average minutes the user was active for a given week', () =>{
     expect(activity.weeklyAvgMins(1, '2019/06/15')).to.equal(171.2);
 
   });
 
-  it('shoud be able know if a user met thier step goal for a specified day', () =>{
+  it('shoud be able know if a user met their step goal for a specified day', () =>{
     expect(activity.stepGoalMet(1, '2019/06/15', userData)).to.equal(false);
   });
 
